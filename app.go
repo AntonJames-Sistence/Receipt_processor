7144a5d504995992e480ae077501976244f1b4c9
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-10-04 13:38:34 -0400

    Add calculatePoints func

--- app.go
+++ app.go
@@ -4,7 +4,7 @@ import (
     "encoding/json"
     "log"
     "net/http"
-    // "regexp"
+    "regexp"
     "strings"
     "github.com/google/uuid"
 )
@@ -86,9 +86,56 @@ func GetPoints(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(response)
 }
 
-// func main() {
-//     http.HandleFunc("/receipts/process", ProcessReceipt)
-//     http.HandleFunc("/receipts/", GetPoints)
+// Calculator for points
+func calculatePoints(receipt Receipt) int {
+    points := 0
 
-//     log.Fatal(http.ListenAndServe(":8080", nil))
-// }
+    // Rule 1: One point for every alphanumeric character in the retailer name.
+    points += len(regexp.MustCompile("[a-zA-Z0-9]").FindAllString(receipt.Retailer, -1))
+
+    // Rule 2: 50 points if the total is a round dollar amount with no cents.
+    total, _ := strconv.ParseFloat(receipt.Total, 64)
+    if total == float64(int(total)) {
+        points += 50
+    }
+
+    // Rule 3: 25 points if the total is a multiple of 0.25.
+    if math.Mod(total, 0.25) == 0 {
+        points += 25
+    }
+
+    // Rule 4: 5 points for every two items on the receipt.
+    points += len(receipt.Items) / 2 * 5
+
+    // Rule 5: If the trimmed length of the item description is a multiple of 3,
+    // multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+    for _, item := range receipt.Items {
+        trimmedLength := len(strings.TrimSpace(item.ShortDescription))
+        if trimmedLength%3 == 0 {
+            price, _ := strconv.ParseFloat(item.Price, 64)
+            points += int(math.Ceil(price * 0.2))
+        }
+    }
+
+    // Rule 6: 6 points if the day in the purchase date is odd.
+    purchaseDate, _ := time.Parse("2006-01-02", receipt.PurchaseDate)
+    if purchaseDate.Day()%2 != 0 {
+        points += 6
+    }
+
+    // Rule 7: 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+    purchaseTime, _ := time.Parse("15:04", receipt.PurchaseTime)
+    if purchaseTime.After(time.Date(0, 0, 0, 14, 0, 0, 0, time.UTC)) && purchaseTime.Before(time.Date(0, 0, 0, 16, 0, 0, 0, time.UTC)) {
+        points += 10
+    }
+
+    return points
+}
+
+
+func main() {
+    http.HandleFunc("/receipts/process", ProcessReceipt)
+    http.HandleFunc("/receipts/{id}/points", GetPoints) // Register the GetPoints handler
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
